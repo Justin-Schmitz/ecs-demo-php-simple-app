cdd73a65a9c3e915db88d18c8560fd20abd2bed0
Justin Schmitz <justin.schmitz@uk.fujitsu.com>
2019-07-26 12:33:39 +0100

    updates to front end

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
+                <h1>Simple stuff init??????</h1>
+                <h2>Congratulations Justin for passing your Exam</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
